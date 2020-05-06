fea63b0911b7873c1132fd1c24b6a5946d7b1dc7
marsdenmadoka <marsmadoka98@gmail.com>
2020-05-06 21:01:20 +0300

    added new files

--- app/src/main/java/marsmadoka98/gmail/ecommerceapp/MainActivity.java
+++ app/src/main/java/marsmadoka98/gmail/ecommerceapp/MainActivity.java
@@ -1,15 +1,30 @@
 package marsmadoka98.gmail.ecommerceapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.app.WallpaperInfo;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import io.paperdb.Paper;
+import marsmadoka98.gmail.ecommerceapp.Model.Users;
+import marsmadoka98.gmail.ecommerceapp.Prevalent.Prevalent;
 
 public class MainActivity extends AppCompatActivity {
     private Button joinNowButton,loginButton;
-
+    ProgressDialog loadingBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
 
         joinNowButton=findViewById(R.id.main_join_now_btn);
         loginButton=findViewById(R.id.main_login_btn);
+        Paper.init(this);
+        loadingBar = new ProgressDialog(this);
 
         joinNowButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,7 +49,24 @@ public class MainActivity extends AppCompatActivity {
                 SendToLogin();
             }
         });
+
+  //working for remember me credentials
+        String UserPhoneKey = Paper.book().read(Prevalent.UserPhoneKey);
+        String UserPasswordKey = Paper.book().read(Prevalent.UserPasswordKey);
+        if (UserPhoneKey != null && UserPasswordKey != null)
+        {
+            if (!TextUtils.isEmpty(UserPhoneKey)  &&  !TextUtils.isEmpty(UserPasswordKey))
+            {
+                AllowAccess(UserPhoneKey, UserPasswordKey);
+
+                loadingBar.setTitle("Already Logged in");
+                loadingBar.setMessage("Please wait.....");
+                loadingBar.setCanceledOnTouchOutside(false);
+                loadingBar.show();
+            }
+        }
     }
+
 
     public void SendToRegister() {
         Intent RegisterIntent = new Intent(MainActivity.this,RegisterActivity.class);
@@ -43,4 +77,53 @@ public class MainActivity extends AppCompatActivity {
         Intent LoginIntent = new Intent(MainActivity.this,LoginActivity.class);
         startActivity(LoginIntent);
     }
+
+
+    //working for remember me credentials
+    private void AllowAccess(final String phone, final String password)
+    {
+        final DatabaseReference RootRef;
+        RootRef = FirebaseDatabase.getInstance().getReference();
+
+
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+                if (dataSnapshot.child("Users").child(phone).exists())
+                {
+                    Users usersData = dataSnapshot.child("Users").child(phone).getValue(Users.class);
+
+                    if (usersData.getPhone().equals(phone))
+                    {
+                        if (usersData.getPassword().equals(password))
+                        {
+                            Toast.makeText(MainActivity.this, "Please wait, you are already logged in...", Toast.LENGTH_SHORT).show();
+                            loadingBar.dismiss();
+
+                            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                            Prevalent.currentOnlineUser = usersData;
+                            startActivity(intent);
+                        }
+                        else
+                        {
+                            loadingBar.dismiss();
+                            Toast.makeText(MainActivity.this, "Password is incorrect.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "Account with this " + phone + " number do not exists.", Toast.LENGTH_SHORT).show();
+                    loadingBar.dismiss();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
 }
---
+++ app/src/main/java/marsmadoka98/gmail/ecommerceapp/Prevalent/Prevalent.java
@@ -0,0 +1,11 @@
+package marsmadoka98.gmail.ecommerceapp.Prevalent;
+
+import marsmadoka98.gmail.ecommerceapp.Model.Users;
+
+public class Prevalent {
+
+    public  static Users currentOnlineUser; //Users here is our Model class Users
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
