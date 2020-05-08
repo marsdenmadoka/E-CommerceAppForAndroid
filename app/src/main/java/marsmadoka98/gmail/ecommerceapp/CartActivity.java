bc86d4b9a125a8a85908eddd0991d595e4e9260c
marsdenmadoka <marsmadoka98@gmail.com>
2020-05-08 23:36:58 +0300

    added new files

---
+++ app/src/main/java/marsmadoka98/gmail/ecommerceapp/CartActivity.java
@@ -0,0 +1,36 @@
+package marsmadoka98.gmail.ecommerceapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class CartActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private Button NextProcessBtn;
+    private TextView txtTotalAmount, txtMsg1;
+    private int overTotalPrice = 0;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+
+        recyclerView = findViewById(R.id.cart_list);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        NextProcessBtn = (Button) findViewById(R.id.next_btn);
+        txtTotalAmount = (TextView) findViewById(R.id.total_price);
+        txtMsg1 = (TextView) findViewById(R.id.msg1);
+
+    }
+}
